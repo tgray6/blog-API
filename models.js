25dac0bdb4abbc2820a64f11cf80a937c266a8fc
Xer0 <tylergrayx@gmail.com>
2017-12-07 21:15:44 -0500

    no clue what I am doing

--- models.js
+++ models.js
@@ -1,4 +1,87 @@
-const uuid = require('uuid');
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    created: this.created
+  };
+};
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
+
+
+
+
+
+
+
+
+
+
+
+//ALL THIS IS MY OLD DATA, NO IDEA WHATS GOING ON, SO I JUST COPIED AND PASTED SOLUTION
+//BUT KEPT OLD DATA
+
+// const uuid = require('uuid');
+
+
+// const mongoose = require("mongoose");
+// mongoose.Promise = global.Promise;
+
+// //lets make a schema with mongoose that will represent our blog data
+// const blogSchema = mongoose.Schema({
+//   author: {
+//     firstName: String,
+//     lastName: String
+//   },
+//   title: {type: String, required: true},
+//   content: {type: String},
+//   created: {type: Date, default: Date.now}
+// });
+
+// blogPostSchema.virtual("authorName").get(function(){
+//   return `${this.author.firstName} ${this.author.lastName}`.trim();
+// });
+
+// blodPostSchema.methods.serialize = function(){
+//   return{
+//     id: this._id,
+//     author: this.authorName,
+//     content: this.content,
+//     title: this.title,
+//     created: this.created
+//   };
+// };
+
+// const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+// module.exports = {BlogPost};
+
+
+
 
 // this module provides volatile storage, using a `BlogPost`
 // model. We haven't learned about databases yet, so for now
@@ -10,61 +93,64 @@ const uuid = require('uuid');
 // is implemented, and getting it to use an existing model.
 
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
-};
+//Below was the volatile storage we wrote in unit 1, you lose this data when server restarts
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
+// function StorageException(message) {
+//    this.message = message;
+//    this.name = "StorageException";
+// }
+
+// const BlogPosts = {
+//   create: function(title, content, author, publishDate) {
+//     const post = {
+//       id: uuid.v4(),
+//       title: title,
+//       content: content,
+//       author: author,
+//       publishDate: publishDate || Date.now()
+//     };
+//     this.posts.push(post);
+//     return post;
+//   },
+//   get: function(id=null) {
+//     // if id passed in, retrieve single post,
+//     // otherwise send all posts.
+//     if (id !== null) {
+//       return this.posts.find(post => post.id === id);
+//     }
+//     // return posts sorted (descending) by
+//     // publish date
+//     return this.posts.sort(function(a, b) {
+//       return b.publishDate - a.publishDate
+//     });
+//   },
+//   delete: function(id) {
+//     const postIndex = this.posts.findIndex(
+//       post => post.id === id);
+//     if (postIndex > -1) {
+//       this.posts.splice(postIndex, 1);
+//     }
+//   },
+//   update: function(updatedPost) {
+//     const {id} = updatedPost;
+//     const postIndex = this.posts.findIndex(
+//       post => post.id === updatedPost.id);
+//     if (postIndex === -1) {
+//       throw StorageException(
+//         `Can't update item \`${id}\` because doesn't exist.`)
+//     }
+//     this.posts[postIndex] = Object.assign(
+//       this.posts[postIndex], updatedPost);
+//     return this.posts[postIndex];
+//   }
+// };
+
+// function createBlogPostsModel() {
+//   const storage = Object.create(BlogPosts);
+//   storage.posts = [];
+//   return storage;
+// }
 
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+// module.exports = {BlogPosts: createBlogPostsModel()};
--- test/test-server.js
+++ test/test-server.js
@@ -54,91 +54,86 @@ describe('Blog Posts', function() {
         });
       });
   });
-});
 
   // test strategy:
   //  1. make a POST request with data for a new item
   //  2. inspect response object and prove it has right
   //  status code and that the returned object has an `id`
+  it('should add a blog item on POST', function() {
+    const newItem = {title: 'foo', content: "string", author: "billy"};
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('title', 'content', 'author');
+        res.body.id.should.not.be.null;
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+       res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}, {publishDate: res.body.publishDate}));
+      });
+  });
+
+  // test strategy:
+  //  1. initialize some update data (we won't have an `id` yet)
+  //  2. make a GET request so we can get an item to update
+  //  3. add the `id` to `updateData`
+  //  4. Make a PUT request with `updateData`
+  //  5. Inspect the response object to ensure it
+  //  has right status code and that we get back an updated
+  //  item with the right data in it.
+  it('should update blog items on PUT', function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      title: 'foo',
+      content: 'string',
+      author: 'billy',
+      publishDate: '103085'
+    };
+
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/blog-posts')
+      .then(function(res) {
+        updateData.id = res.body[0].id
+        // updateData.publishDate=res.body[0].publishDate
+        // this will return a promise whose value will be the response
+        // object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of
+        // returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/blog-posts/${updateData.id}`)
+          // .put(`/blog-posts/${updateData.publishDate}`)
+          .send(updateData);
+      })
+      // prove that the PUT request has right status code
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+  // test strategy:
+  //  1. GET blog items so we can get ID of one
+  //  to delete.
+  //  2. DELETE an item and ensure we get back a status 204
+  it('should delete blog items on DELETE', function() {
+    return chai.request(app)
+      // first have to get so we have an `id` of item
+      // to delete
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
 
 
-
-
-
-
-//   it('should add a blog item on POST', function() {
-//     const newItem = {title: 'foo', content: "string", author: "billy"};
-//     return chai.request(app)
-//       .post('/blog-posts')
-//       .send(newItem)
-//       .then(function(res) {
-//         res.should.have.status(201);
-//         res.should.be.json;
-//         res.body.should.be.a('object');
-//         res.body.should.include.keys('title', 'content', 'author');
-//         res.body.id.should.not.be.null;
-//         // response should be deep equal to `newItem` from above if we assign
-//         // `id` to it from `res.body.id`
-//        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}, {publishDate: res.body.publishDate}));
-//       });
-//   });
-
-//   // test strategy:
-//   //  1. initialize some update data (we won't have an `id` yet)
-//   //  2. make a GET request so we can get an item to update
-//   //  3. add the `id` to `updateData`
-//   //  4. Make a PUT request with `updateData`
-//   //  5. Inspect the response object to ensure it
-//   //  has right status code and that we get back an updated
-//   //  item with the right data in it.
-//   it('should update blog items on PUT', function() {
-//     // we initialize our updateData here and then after the initial
-//     // request to the app, we update it with an `id` property so
-//     // we can make a second, PUT call to the app.
-//     const updateData = {
-//       title: 'foo',
-//       content: 'string',
-//       author: 'billy',
-//       publishDate: '103085'
-//     };
-
-//     return chai.request(app)
-//       // first have to get so we have an idea of object to update
-//       .get('/blog-posts')
-//       .then(function(res) {
-//         updateData.id = res.body[0].id
-//         // updateData.publishDate=res.body[0].publishDate
-//         // this will return a promise whose value will be the response
-//         // object, which we can inspect in the next `then` back. Note
-//         // that we could have used a nested callback here instead of
-//         // returning a promise and chaining with `then`, but we find
-//         // this approach cleaner and easier to read and reason about.
-//         return chai.request(app)
-//           .put(`/blog-posts/${updateData.id}`)
-//           // .put(`/blog-posts/${updateData.publishDate}`)
-//           .send(updateData);
-//       })
-//       // prove that the PUT request has right status code
-//       .then(function(res) {
-//         res.should.have.status(204);
-//       });
-//   });
-
-//   // test strategy:
-//   //  1. GET blog items so we can get ID of one
-//   //  to delete.
-//   //  2. DELETE an item and ensure we get back a status 204
-//   it('should delete blog items on DELETE', function() {
-//     return chai.request(app)
-//       // first have to get so we have an `id` of item
-//       // to delete
-//       .get('/blog-posts')
-//       .then(function(res) {
-//         return chai.request(app)
-//           .delete(`/blog-posts/${res.body[0].id}`);
-//       })
-//       .then(function(res) {
-//         res.should.have.status(204);
-//       });
-//   });
-// });
